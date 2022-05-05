e043fafc08401ad77cff2375a321461a2e7a053f
ashit <ashitkoladiya7@gmail.com>
2022-05-05 08:28:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,23 +2,119 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const emp =[
+    {
+      name: 'amit',
+      age: 35,
+      salary: 40000,
+      bonus:1000,
+      status: true
+    },
+    {
+      name: 'ajay',
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: 'mayur',
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: 'jay',
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: 'raj',
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    }
+  ];
+  
+  // destructure
+  
+  let[name]=emp;
+  console.log(name);
+  
+  console.log(emp.map((i) => {return i})); 
+  // emp.map((d,i) => console.log(d.id,d.name));
+  
+  // console.log(emp.filter((f) => {return f.expiry >= 2022}));
+  
+  let em = emp.filter((e) => {return e.status >= true});
+  console.log(em);
+  
+  let emans = em.reduce((acc, emp, i) => acc + emp.bonus + emp.salary, 0);
+  console.log(emans);
+  
+  let eman = emp.reduce((acc, emp, i) => acc + emp.bonus + emp.salary, 0);
+  console.log(eman);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <center>
+        <h4>Employee Data</h4>
+    <table border = "1">
+        <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
+        <th>Total</th>
+        </tr>
+        <tbody>
+          {
+            emp.map((emp,index) => {
+              return(
+                <tr>
+                  <td>{emp.name}</td>
+                  <td>{emp.age}</td>
+                  <td>{emp.salary}</td>
+                  <td>{emp.bonus}</td>
+                  {index === 0 ? <td rowspan = {emp.length = "5"} > {eman} </td> : null} 
+                </tr>
+              )
+            })
+          }
+        </tbody>
+    </table>
+
+    <br></br>
+        <h4>Employee Data-1</h4>
+    <table border = "1">
+        <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
+        <th>Total</th>
+        </tr>
+        <tbody>
+          {
+            em.map((emp,index) => {
+              return(
+                <tr>
+                  <td>{emp.name}</td>
+                  <td>{emp.age}</td>
+                  <td>{emp.salary}</td>
+                  <td>{emp.bonus}</td>
+                  {index === 0 ? <td rowspan = {emp.length = "5"} > {emans} </td> : null} 
+                </tr>
+              )
+            })
+          }
+        </tbody>
+    </table>
+    </center>
+    </>
   );
 }
 
